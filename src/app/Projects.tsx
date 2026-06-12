6fd8170f251134a4a572205129711116e8f4ca76
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-06-12 17:56:32 +0530

    feat: added heading to projects screen and created component file

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -1,13 +1,51 @@
+import Lucide from '@react-native-vector-icons/lucide'
 import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Projects = () => {
   return (
-    <View>
-      <Text>Projects</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.headingContainer}>
+        <View>
+          <Text style={styles.todaysSummaryText}>My Projects</Text>
+          <Text style={styles.totalProjectText}>12 Projects</Text>
+        </View>
+      <Lucide name="search" color={"white"} size={24}/>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Projects
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#13131A",
+    paddingHorizontal: 16
+  },
+  headingContainer: {
+    width: "100%",
+    height: 74,
+    borderBottomWidth: 0.3,
+    borderBottomColor: "#464646",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingBottom: 12,
+    paddingRight: 14
+  },
+  todaysSummaryText: {
+    fontSize: 20,
+    fontFamily: "GeistMedium",
+    fontWeight: "100",
+    color: "white",
+    marginTop: 8
+  },
+  totalProjectText: {
+    fontSize: 16,
+    fontFamily: "GeistMedium",
+    fontWeight: "100",
+    color: "#828282",
+  },
+})
---
+++ src/components/ProjectListItem.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Project = () => {
+  return (
+    <View>
+      <Text>Project</Text>
+    </View>
+  )
+}
+
+export default Project
+
+const styles = StyleSheet.create({})
