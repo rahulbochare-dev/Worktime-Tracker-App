6966e4b966e7e869241b5ec21e02d8bfc70d80b8
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-06-26 20:01:59 +0530

    feat: changed ui of start session modal and added duntion to get id of project clicked

--- src/components/StartSessionModal.tsx
+++ src/components/StartSessionModal.tsx
@@ -1,6 +1,7 @@
-import { Modal, StyleSheet, Text, View } from "react-native";
+import { Modal, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
 import Button from "./Button";
-import Dropdown from "./Dropdown";
+import { useProjectStore } from "@/store/projects.store";
 
 type props = {
   visible: boolean,
@@ -9,6 +10,20 @@ type props = {
 };
 
 const StartSessionModal = ({ visible, startFunc, cancelFunc }: props) => {
+  const { projects, getProjects } = useProjectStore()
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const response = await getProjects()
+    }
+    fetchProjects()
+  }, [])
+
+  const onSelect = (id: number) => {
+    console.log(id)
+    cancelFunc()
+  }
+
   return (
     <Modal
       visible={visible}
@@ -16,10 +31,22 @@ const StartSessionModal = ({ visible, startFunc, cancelFunc }: props) => {
       animationType="fade">
       <View style={styles.backdrop}>
         <View style={styles.modalContainer}>
-          <Text style={styles.titleText}>Start New Session</Text>
-          <Dropdown/>
-          <Button title={"Start Session"} primary={true} disabled={false} width={"100%"} func={startFunc}/>
-          <Button title={"Cancel"} primary={false} disabled={false} width={"100%"} func={cancelFunc}/>
+          <Text style={styles.titleText}>Select Project</Text>
+          <ScrollView style={styles.listItemContainerMain}>
+            {projects?.map((items) => {
+              return <Pressable style={styles.projectDeatils} key={items.id} onPress={()=> onSelect(items.id)}>
+                <View style={styles.listItemContainer}>
+                  <View style={styles.projectDeatilsIcon}>
+                    <Text style={styles.projectDeatilsIconText}>{items.name.slice(0, 1).toUpperCase()}</Text>
+                  </View>
+                  <View>
+                    <Text style={styles.projectDeatilsHeadings}>{items?.name}</Text>
+                  </View>
+                </View>
+              </Pressable>
+            })}
+          </ScrollView>
+          <Button title={"Cancel"} primary={false} disabled={false} width={"100%"} func={cancelFunc} />
         </View>
       </View>
     </Modal>
@@ -38,24 +65,60 @@ const styles = StyleSheet.create({
   },
   modalContainer: {
     width: "90%",
-    maxWidth: 380,
+    maxHeight: "70%",
     backgroundColor: "#090913",
     borderRadius: 16,
     borderWidth: 0.3,
     borderColor: "#464646",
-    padding: 18,
-    justifyContent: "center",
+    paddingHorizontal: 18,
+    paddingVertical: 22,
+    justifyContent: "space-between",
     alignItems: "center",
-    gap: 16
+    gap: 16,
   },
   titleText: {
     fontSize: 20,
     fontFamily: "GeistMedium",
     color: "white",
   },
-  projectPlaceholder: {
-    color: "#8A8A8A",
+  listItemContainerMain: {
+    width: "100%",
+    maxHeight: "80%",
+  },
+  listItemContainer: {
+    width: "100%",
+    height: 64,
+    borderBottomWidth: 0.3,
+    borderBottomColor: "#464646",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 4,
+    gap: 14
+  },
+  projectDeatils: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 14,
+  },
+  projectDeatilsIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+    backgroundColor: "#8282FF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  projectDeatilsIconText: {
+    fontSize: 18,
+    fontFamily: "GeistMedium",
+    fontWeight: "100",
+    color: "white",
+  },
+  projectDeatilsHeadings: {
     fontSize: 16,
-    fontFamily: "GeistRegular",
+    fontFamily: "GeistMedium",
+    fontWeight: "100",
+    color: "white",
   }
 });
