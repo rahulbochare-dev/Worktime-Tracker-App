7880a7b3bb002b62ea00a71177ed084e0e0a6f7f
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-07-06 13:12:44 +0530

    feat: created ui for set goal and created component

--- src/app/(tabs)/Settings.tsx
+++ src/app/(tabs)/Settings.tsx
@@ -1,13 +1,44 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SetGoal from '../../components/SetGoal'
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.headingContainer}>
+        <View>
+          <Text style={styles.heading1Text}>Settings</Text>
+        </View>
+      </View>
+      <SetGoal/>
+    </SafeAreaView>
   )
 }
 
 export default Settings
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#0d0d0d",
+    paddingHorizontal: 16,
+  },
+  headingContainer: {
+    width: "100%",
+    height: 74,
+    borderBottomWidth: 0.3,
+    borderBottomColor: "#464646",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingBottom: 12,
+    paddingRight: 14
+  },
+  heading1Text: {
+    fontSize: 20,
+    fontFamily: "GeistMedium",
+    fontWeight: "100",
+    color: "white",
+    marginTop: 8
+  }
+})
---
+++ src/components/SetGoal.tsx
@@ -0,0 +1,42 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+
+const SetGoal = () => {
+  const [time, setTime] = useState({
+    hours: null,
+    minutes: null
+  })
+  return (
+    <Pressable style={styles.container}>
+      <Text style={styles.setGoalText}>SetGoal</Text>
+      <Text style={styles.goalNoText}>4hr 33min</Text>
+    </Pressable>
+  )
+}
+
+export default SetGoal
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 64,
+    backgroundColor: "#151515",
+    borderRadius: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 16
+  },
+  setGoalText: {
+    fontSize: 16,
+    fontFamily: "GeistMedium",
+    fontWeight: "100",
+    color: "white",
+  },
+  goalNoText: {
+    fontSize: 16,
+    fontFamily: "GeistMedium",
+    fontWeight: "100",
+    color: "#828282",
+  }
+})
