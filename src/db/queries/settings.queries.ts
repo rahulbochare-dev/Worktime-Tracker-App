b72ed97994805a8dc60371dd14465f4937ecf711
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-07-19 11:54:53 +0530

    refactor: added check to insert only one value

--- src/db/queries/settings.queries.ts
+++ src/db/queries/settings.queries.ts
@@ -2,7 +2,15 @@ import { db } from "../index";
 import { eq } from "drizzle-orm";
 import { settings } from "../schema";
 
-export const createGoalQuery = async (timeMS) => {
+export const createGoalQuery = async (timeMS: number) => {
+  const existing = await db.select().from(settings)
+
+  if(existing.length > 0){
+    return await db.update(settings).set({
+      dailyGoal: timeMS
+    })
+  }
+
   const response = await db.insert(settings).values({
     dailyGoal: timeMS
   })
