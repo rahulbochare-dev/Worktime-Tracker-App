2f3294c498ef772ec262caa7153f8884f20c7acb
Rahul Bochare <rahulbochare.dev@gmail.com>
2026-07-19 11:47:08 +0530

    feat: made query for gitting dailyGoal

--- src/db/queries/settings.queries.ts
+++ src/db/queries/settings.queries.ts
@@ -1,10 +1,17 @@
 import { db } from "../index";
+import { eq } from "drizzle-orm";
 import { settings } from "../schema";
 
 export const createGoalQuery = async (timeMS) => {
   const response = await db.insert(settings).values({
     dailyGoal: timeMS
   })
+
+  return response;
+}
+
+export const getGoalQuery = async () => {
+  const response = await db.select().from(settings)
 
   return response;
 }
